4fb8a2b856c05f18a007026dc3075517bc20db7b
AdriaLopezGonzalez <97537126+AdriaLopezGonzalez@users.noreply.github.com>
2022-06-08 17:12:34 +0100

    super

---
+++ student/SFML_TCengine/Source/Game/LaserWeaponComponent.cs
@@ -0,0 +1,83 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using TCEngine;
+
+namespace TCGame
+{
+    public class LaserWeaponComponent : BaseComponent
+    {
+        private const float DEFAULT_FIRE_RATE = 0.3f;
+
+        private float m_FireRate;
+        private float m_TimeToShoot = 0.0f;
+
+        public LaserWeaponComponent()
+        {
+            m_FireRate = DEFAULT_FIRE_RATE;
+            m_TimeToShoot = 0.0f;
+        }
+
+        public LaserWeaponComponent(float _fireRate)
+        {
+            m_FireRate = _fireRate;
+            m_TimeToShoot = 0.0f;
+        }
+
+        public override void OnActorCreated()
+        {
+            base.OnActorCreated();
+        }
+
+        public override void Update(float _dt)
+        {
+            base.Update(_dt);
+
+            // TODO (3): Remember to update the m_TimeToShoot member
+            if (m_TimeToShoot > 0.0f)
+            {
+                m_TimeToShoot -= _dt;
+            }
+        }
+
+        public void Shoot()
+        {
+            // TODO (3): Implement the creation of the laser bullet. Remember that the LaserWeaponComponent has a fire rate of 0.3 seconds
+            if (m_TimeToShoot <= 0.0f)
+            {
+                Actor laserActor = new Actor("Laser Actor");
+
+                SpriteComponent spriteComponent = laserActor.AddComponent<SpriteComponent>("Textures/Bullet");
+                spriteComponent.m_RenderLayer = RenderComponent.ERenderLayer.Middle;
+
+                TransformComponent actorTransform = Owner.GetComponent<TransformComponent>();
+                TransformComponent transformComponent = laserActor.AddComponent<TransformComponent>();
+                transformComponent.Transform.Position = actorTransform.Transform.Position;
+                RenderWindow _Window = TecnoCampusEngine.Get.Window;
+                Vector2f _mouseForward = new Vector2f(Mouse.GetPosition(_Window).X, Mouse.GetPosition(_Window).Y);
+                Vector2f _bulletForward = _mouseForward - transformComponent.Transform.Position;
+
+                laserActor.AddComponent<ForwardMovementComponent>(_bulletForward.Normal(), 500.0f);
+
+                laserActor.AddComponent<AsteroidDestructorComponent>();
+                laserActor.AddComponent<ExplosionComponent>();
+                laserActor.AddComponent<OutOfWindowDestructionComponent>();
+
+                TecnoCampusEngine.Get.Scene.CreateActor(laserActor);
+
+                m_TimeToShoot = m_FireRate;
+            }
+        }
+
+        public override EComponentUpdateCategory GetUpdateCategory()
+        {
+            return EComponentUpdateCategory.PostUpdate;
+        }
+
+        public override object Clone()
+        {
+            LaserWeaponComponent clonedComponent = new LaserWeaponComponent(m_FireRate);
+            return clonedComponent;
+        }
+    }
+}
---
+++ student/SFML_TCengine/Source/Game/ShieldComponent.cs
@@ -0,0 +1,180 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using System.Diagnostics;
+using TCEngine;
+
+namespace TCGame
+{
+    public class ShieldComponent : BaseComponent
+    {
+
+        private const float LOADING_TIME = 0.2f;
+        private const float LOADED_TIME = 5.0f;
+        private const float UNLOADING_TIME = 0.2f;
+
+        private const float MIN_SCALE = 0.1f;
+        private const float MAX_SCALE = 1.0f;
+
+        private Actor m_ShieldActor;
+
+        private enum EState
+        {
+            Inactive,
+            Loading,
+            Loaded,
+            Unloading
+        }
+
+        private EState m_CurrentState;
+        private float m_CurrentStateTime = 0.0f;
+
+        public ShieldComponent()
+        {
+        }
+
+        public override void OnActorCreated()
+        {
+            base.OnActorCreated();
+            m_CurrentState = EState.Inactive;
+        }
+
+        public void ActivateShield()
+        {
+            if (m_CurrentState == EState.Inactive)
+            {
+                ChangeState(EState.Loading);
+            }
+        }
+
+        public bool IsActive()
+        {
+            return m_CurrentState != EState.Inactive;
+        }
+
+
+
+        public override void Update(float _dt)
+        {
+            base.Update(_dt);
+
+            // TODO (1): You need to implement the code that update the different states of the ShieldComponent in the Update
+            switch (m_CurrentState)
+            {
+                case EState.Inactive:
+                    UpdateInactive();
+                    break;
+                case EState.Loading:
+                    UpdateLoading(_dt);
+                    break;
+                case EState.Loaded:
+                    UpdateLoaded(_dt);
+                    break;
+                case EState.Unloading:
+                    UpdateUnloading(_dt);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void UpdateInactive()
+        {
+        }
+
+        private void UpdateLoading(float _dt)
+        {
+            m_CurrentStateTime += _dt;
+
+            float actualScale = MathUtil.Lerp(MIN_SCALE, MAX_SCALE, m_CurrentStateTime / 0.2f);
+
+            m_ShieldActor.GetComponent<TransformComponent>().Transform.Scale = new Vector2f(actualScale, actualScale);
+
+            if (m_CurrentStateTime >= LOADING_TIME)
+            {
+                ChangeState(EState.Loaded);
+                m_CurrentStateTime = 0;
+            }
+        }
+
+        private void UpdateLoaded(float _dt)
+        {
+            m_CurrentStateTime += _dt;
+
+            if(m_CurrentStateTime >= LOADED_TIME)
+            {
+                ChangeState(EState.Unloading);
+                m_CurrentStateTime = 0;
+            }
+        }
+
+        private void UpdateUnloading(float _dt)
+        {
+            m_CurrentStateTime += _dt;
+
+            float actualScale = MathUtil.Lerp(MIN_SCALE, MAX_SCALE, (UNLOADING_TIME - m_CurrentStateTime) / 0.2f);
+
+            m_ShieldActor.GetComponent<TransformComponent>().Transform.Scale = new Vector2f(actualScale, actualScale);
+
+            if (m_CurrentStateTime >= UNLOADING_TIME)
+            {
+                ChangeState(EState.Inactive);
+                m_CurrentStateTime = 0;
+            }
+        }
+
+        private void CreateShieldActor()
+        {
+            m_ShieldActor = new Actor("Shield Actor");
+
+            m_ShieldActor.AddComponent<AnimatedSpriteComponent>("Textures/Shield", 3u, 2u);
+            m_ShieldActor.AddComponent<TransformComponent>().Transform.Scale = new Vector2f(0, 0);
+            m_ShieldActor.AddComponent<ParentActorComponent>(Owner, new Vector2f(0.0f, 20.0f));
+
+            TecnoCampusEngine.Get.Scene.CreateActor(m_ShieldActor);
+        }
+
+        private void ChangeState(EState _newState)
+        {
+            OnLeaveState(m_CurrentState);
+            OnEnterState(_newState);
+
+            m_CurrentState = _newState;
+        }
+
+        private void OnEnterState(EState _nextState)
+        {
+            switch (_nextState)
+            {
+                case EState.Loading:
+                    CreateShieldActor();
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void OnLeaveState(EState _previousState)
+        {
+            switch (_previousState)
+            {
+                case EState.Unloading:
+                    m_ShieldActor.Destroy();
+                    m_ShieldActor = null;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public override EComponentUpdateCategory GetUpdateCategory()
+        {
+            return EComponentUpdateCategory.Update;
+        }
+
+        public override object Clone()
+        {
+            ShieldComponent clonedComponent = new ShieldComponent();
+            return clonedComponent;
+        }
+    }
+}
